cc73035c173a58c4b839ea6111b595c303d26e47
warddam001 <warddam001@warren.k12.in.us>
2021-08-09 12:58:42 -0400

    push

--- src/L3.java
+++ src/L3.java
@@ -1,4 +1,44 @@
 public class L3 {
 
     //add the main method header below.
-}
+
+/*
+Literal Value is a value written directly into a code.
+An Expression is a value written using variables or operators.
+ */
+
+
+    public static void main(String[] args){
+
+        int literalValue = 5;
+        int expression = 5 + 6;
+        int expression2 = literalValue + 6;
+        int expression3 = literalValue + literalValue;
+
+             //Examples of Literal Values
+               double literalValue2 = 26;
+               boolean literalValue3 = false;
+               String literalValue4 = "People";
+
+        //Examples of Expressions - the result of expression must match the data type
+          double expression4 = 2.4 + 3.6;
+               boolean expression5 = 5 > 6;
+
+               //int badExample = 5.4-0.4;
+
+    //String expression is called STRING CONCATENATION. You can combine Strings using the +.
+
+    String expression6 = "A movie theater is full of " + literalValue4;
+            System.out.println(expression6);
+
+       
+
+            String message1 = "The store has ";
+            String message2 = " apples in stock.";
+            int numApples = 20;
+
+            System.out.println("The store has 20 apples in stock.");
+            System.out.println("The store has " + numApples + " apples in stock");
+            System.out.println(message1 + numApples + message2);
+        }
+    }
--- src/L5.java
+++ src/L5.java
@@ -1,2 +1,37 @@
 public class L5 {
+    public static void main(String[] args){
+
+        double basePay = 7.25;
+        double hoursWeek1 = 16.5;
+        double hoursWeek2 = 23.75;
+
+        double totalHours = hoursWeek1 + hoursWeek2;
+        double wages = basePay * totalHours;
+
+        System.out.println("Your bi-weekly paycheck is: $" + wages);
+        int fraction = 1/2;
+        System.out.println(fraction);
+        //Modules is the remainder of a division problem
+        //25 % 6 = 1
+        //17 % 3 = 2
+        //4 % 2 = 0
+        //6 % 2 = 0
+        //121 % 100 = 21
+        //5 % 2 = 1
+        //47 % 15 = 2
+        //55 % 15 = 10
+
+        //
+        //
+        //
+        //
+        //
+
+        System.out.println(5 + 2 * 4);
+        System.out.println(12/2 - 4);
+        System.out.println(3 + 17 % 2 - 1);
+        System.out.println(4 + 5 * 2 / 2 +1);
+
+    }
 }
+
